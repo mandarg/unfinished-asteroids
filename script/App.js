3a597b1d1f5631c60084d9b8cb6d4f739e969060
Mandar Gokhale <mandar.mmg@gmail.com>
2015-04-27 18:31:18 -0400

    Complete chapter 2

--- script/App.js
+++ script/App.js
@@ -1,11 +1,11 @@
 var app = playground({
 
-  width: 320,
-  height: 240,
+  width: 640,
+  height: 480,
 
-  scaleToFit: false,
+  scaleToFit: true,
 
-  smoothing: true,
+  smoothing: false,
 
   create: function() {
 
