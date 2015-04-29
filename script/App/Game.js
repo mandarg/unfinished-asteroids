29c74c9e400c5953c825f6735b724c2929446ec6
Mandar Gokhale <mandar.mmg@gmail.com>
2015-04-28 22:59:16 -0400

    Complete Chapter 3

--- script/App/Game.js
+++ script/App/Game.js
@@ -105,7 +105,7 @@ app.game = {
         this.players[0].left = false;
         break;
       case "right":
-        this.players[0].left = false  ;
+        this.players[0].right = false  ;
         break;
       case "up":
         this.players[0].up = false;
